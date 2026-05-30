94d9908e05c0d0e79f05760722c37c4a9e7b6417
Sarinyatee <sarinyatee@gmail.com>
2026-05-30 14:03:06 -0700

    Fixed toString, eliminate, added JosephusSim, add
    - Fixed toString() to handles fencepost comma.
    - JosephusSim(String fileName): 
    The constructor will likely be your longest method in this assignment. It should:
    
    Read all the names out of one of the provided text file (or one of your own) and create a linked list of PersonNode's with each person's name
    I recommend calling a helper method inside of your file processing loop to add the new name in a new node at the end of your list. This will be similar-ish to the add methods in 6c Reviewing ListNode and LinkedList except that we're working with PersonNodes.
    Or, a more efficient solution, is to generate the list in reverse order, where the last person in the file ends up being at the "front" of the list
    Then the singly linked list should be turned into a circularly linked list by pointing the last node's next at the first node
    Generate and print an elimination count
    This count should not be larger than half the number of people in the list and it must be at least 1. (i.e. If there are 10 people in the list, this value should be a random number 1-5)
    - add((String val) : This private helper method handles adding a new person to the end of the singly linked list during file processing.

--- JosephusSim.java
+++ JosephusSim.java
@@ -11,14 +11,27 @@ public class JosephusSim {
       try {
          // load names from the file in order, generating a singly linked list of PersonNodes
          Scanner file = new Scanner(new File(fileName));
-         
+         size = 0;
          // make the ring circular by attaching last node's next to front
-         
+         while(file.hasNextLine()) {
+            String name = file.nextLine().trim();
+            if(!name.isEmpty()) {
+               add(name);
+            }
+         }
+         file.close();
+         PersonNode last = circle;
+         while(last.next != null) {
+            last = last.next;
+         }   
+         last.next = circle;   
          // remember the last node as the one in front of the next to get eliminated
+         track = last;
          
          // generate, print, and save the random elimination count
          Random rand = new Random();
          eliminationCount = rand.nextInt(size / 2) + 1;
+         System.out.println("=== Elimination count is " + eliminationCount + " ===");
       } catch(FileNotFoundException e) {
          System.out.println("Something went wrong with " + fileName);
       }
@@ -26,35 +39,38 @@ public class JosephusSim {
    
    // optional helper method for constructing the circle
    private void add(String val) {
+      PersonNode newNode = new PersonNode(val);
+      
+      if(circle == null) {
+         circle = newNode;
+      } else {
+         PersonNode cur = circle;
+         while(cur.next != null) {
+            cur = cur.next;
+         }
+         cur.next = newNode;
+      }
+      size++;
    }
    
    public void eliminate() {
-      PersonNode eliminateName = null;
+      if(size <= 1) {
+         return;
+      }
+     
       PersonNode cur = circle;
-      // count to the elimination count
-      for(int i = 0; i < eliminationCount; i++) {
-         eliminateName = circle.next;   
+      // Get last
+      for(int i = 1; i < eliminationCount - 1; i++) {
+         cur = cur.next;   
       } 
-      //Get last
-      while(cur.next != null) {
-         cur = cur.next;
-      }
       PersonNode last = cur;
-      cur = circle;
-      //Get new last
-      for(int i = 0; i < eliminationCount - 1; i++) {
-         cur = cur.next;
-      }
-      PersonNode newLast = cur;
-      
+      PersonNode eliminationNode = last.next; 
       // print who will be eliminated
-      System.out.println(eliminateName + " eliminated!");
-      // eliminate the person and update "front" of the circle and size
-      last.next = circle;                       // point last to front
-      PersonNode newFront = eliminateName.next; // get new front
-      newLast.next = newFront;                  // point new last to new front
+      System.out.println(eliminationNode.name + " eliminated!");
+      PersonNode newFront = eliminationNode.next;
+      last.next = newFront;
       circle = newFront;
-      
+      size--;      
    }
    
    public boolean isOver() {
@@ -63,14 +79,20 @@ public class JosephusSim {
    }
    
    public String toString() {
+      PersonNode cur = circle;
       String result = "";
       // if there's only one person left, print them as the last survivor
       if(size == 1) {
          result = circle.name + " is the last survivor!";
       }
       // print the remaining survivors (watch out for infinite loop since list is circular)
+      result = "Remaining survivors: ";
       for(int i = 1; i <= size; i++) {
-         result = i + "-" + circle.name + ", ";
+         result += i + "-" + cur.name;
+         if(i < size) {
+            result += ", ";
+         }
+         cur = cur.next;   
       }
       return result;
    }
